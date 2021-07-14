28d1187a0cd2bf1b24738301d0b04f7b711e591b
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-14 20:08:35 -0300

    build(main.js/index.html): Linguagem da aplicação alterada, main.js alterado para rodar os plugins

--- src/main.js
+++ src/main.js
@@ -1,7 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
+import http from "./http";
+import VueRouter from 'vue-router';
+import provider from './provider';
+import { routes } from './routes';
+import moment from 'moment'
+import vuetify from './plugins/vuetify'
+import { library } from '@fortawesome/fontawesome-svg-core'
+//import { faBars} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.use(VueRouter);
+
+library.add()
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+
+Vue.prototype.$http = http;
 
 new Vue({
   el: '#app',
+  vuetify,
+  router,
+  components: { App },
+  store: provider,
   render: h => h(App)
 })
