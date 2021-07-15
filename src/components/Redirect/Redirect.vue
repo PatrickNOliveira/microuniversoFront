6558d028caec18bcb4856bf4bc965dcbc908b562
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-15 00:36:28 -0300

    feature(Redirect.Vue): Lógica para fazer as urls curtas funcionarem aplicada

---
+++ src/components/Redirect/Redirect.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row align="center" justify="center" class="mt-10">
+  <v-progress-circular
+    indeterminate
+    color="black"
+    class="mt-16"
+  ></v-progress-circular>
+  </v-row>
+</template>
+<script>
+export default {
+  name: "Redirect",
+  beforeCreate() {
+    this.$http.get('/url/'+this.$route.params.code)
+      .then((r) => {
+        if (r.data.destiny){
+          window.location.href = r.data.destiny
+        } else {
+          this.$router.push({name:'notFound'})
+        }
+      })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import Home from "./components/Home/Home";
+import Redirect from "./components/Redirect/Redirect";
+import notFound from './components/404/notFound'
 
 
 export const routes = [
@@ -7,5 +9,14 @@ export const routes = [
     path: '/',
     component: Home,
     alias: '/home'
+  },
+  {
+    path: '/:code',
+    component: Redirect
+  },
+  {
+    path: '/404',
+    component: notFound,
+    name: 'notFound'
   }
 ];
